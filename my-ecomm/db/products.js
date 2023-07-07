cf64536fc405b4cd9aeee3e94b107d9692aa618f
zaidkhan01 <zaidkhan.a888@gmail.com>
2023-07-07 10:25:50 +0530

    making product via js

--- my-ecomm/db/products.js
+++ my-ecomm/db/products.js
@@ -2,7 +2,7 @@ import {v4 as uuid} from "https://jspm.dev/uuid";
 
 export const products = [
     {
-      _id: uuid(),
+      _id: "001",
       name: "Men Checked Shirt",
       img: "https://rukminim1.flixcart.com/image/880/1056/kwtkxow0/shirt/c/u/n/xl-brd-white-p-v-creations-original-imag9f3yxz7d89t4.jpeg?q=50",
       alt: "mens shirt",
@@ -15,7 +15,7 @@ export const products = [
       rating: 4.2,
     },
     {
-      _id: uuid(),
+      _id: "002",
       name: "Women Crop Top",
       img: "https://rukminim1.flixcart.com/image/880/1056/krf91u80/top/g/n/v/xs-lbt-13-maroon-london-belly-original-imag57qgzcm3hsmv.jpeg?q=50",
       alt: "womens top",
@@ -28,7 +28,7 @@ export const products = [
       rating: 4.5,
     },
     {
-      _id: uuid(),
+      _id: "003",
       name: "Women Solid Shirt",
       img: "https://rukminim1.flixcart.com/image/880/1056/kf2v3ww0/top/z/f/t/l-044-yukon01-original-imafvhkejzfhqaqz.jpeg?q=50",
       alt: "Women shirt",
@@ -41,7 +41,7 @@ export const products = [
       rating: 3.9,
     },
     {
-      _id: uuid(),
+      _id: "004",
       name: "Men Solid Shirt",
       brand: "Peter England",
       img: "https://rukminim1.flixcart.com/image/580/696/kwzap3k0/shirt/r/q/j/l-lstr-acqua-p-v-creations-original-imag9jggj6naa4cu.jpeg?q=50",
@@ -54,7 +54,7 @@ export const products = [
       rating: 3.5,
     },
     {
-      _id:uuid(),
+      _id: "005",
       name: "Men Solid T-Shirt",
       brand: "Roadster",
       img: "https://rukminim1.flixcart.com/image/580/696/kz8qsnk0/t-shirt/g/z/n/m-db1024-42-3bros-original-imagbawh38j3ghzr.jpeg?q=50",
@@ -67,7 +67,7 @@ export const products = [
       rating: 3.2,
     },
     {
-      _id: uuid(),
+      _id: "006",
       name: "Women Printed Top",
       img: "https://rukminim1.flixcart.com/image/580/696/kzhbfrk0/top/v/b/6/m-fancy-top-shree-villa-original-imagbhgpqkj9snta.jpeg?q=50",
       alt: "Women Printed Top",
@@ -80,7 +80,7 @@ export const products = [
       rating: 3.2,
     },
     {
-      _id:uuid(),
+      _id: "007",
       name: "Men Regular V Neck T-Shirt",
       img: "https://rukminim1.flixcart.com/image/580/696/k6gsk280/t-shirt/e/z/h/xl-pp-374-385-379-revesto-original-imafkxwnetcxt4xr.jpeg?q=50",
       alt: "mens t-shirt",
@@ -93,7 +93,7 @@ export const products = [
       rating: 2.2,
     },
     {
-      _id: uuid(),
+      _id: "008",
       name: "Women Regular Top",
       img: "https://rukminim1.flixcart.com/image/580/696/knni7ww0/top/o/m/o/s-dmp303-dmp-fashion-original-imag2a5qkjgjf22k.jpeg?q=50",
       alt: "Women Regular Top",
@@ -106,7 +106,7 @@ export const products = [
       rating: 2.2,
     },
     {
-      _id: uuid(),
+      _id: "009",
       name: "Women Solid T-Shirt",
       img: "https://rukminim1.flixcart.com/image/580/696/jzn0tjk0/t-shirt/t/y/g/s-fw-014-mrrd-s-first-wave-original-imaf2nxuqatamwhm.jpeg?q=50",
       alt: "Women t-shirt",
--- my-ecomm/index.js
+++ my-ecomm/index.js
@@ -13,4 +13,69 @@ for(let product of products){
         "relative",
         "shadow"
     );
+
+
+    /*image container */
+    const imageContainer=document.createElement("div");
+    imageContainer.classList.add("card-image-container")
+  
+    const image=document.createElement("img");
+    image.classList.add("card-image");
+    image.setAttribute("src",product.img);
+    image.setAttribute("alt",product.name);
+    imageContainer.appendChild(image);
+
+    /** card details container */
+    const cardDetailsContainer=document.createElement("div");
+    cardDetailsContainer.classList.add("card-details");
+
+    const brandContainer=document.createElement("div");
+    brandContainer.classList.add("card-title");
+    brandContainer.innerText=product.brand;
+    cardDetailsContainer.appendChild(brandContainer)
+
+// card description container
+const descriptionContainer=document.createElement("div");
+descriptionContainer.classList.add("card-description");
+
+const name=document.createElement("p");
+name.classList.add("card-des");
+name.innerText=product.name;
+descriptionContainer.appendChild(name);
+
+// product price
+
+const price=document.createElement("p");
+price.classList.add("card-price");
+price.innerText=`Rs. ${product.newPrice}`;
+
+const oldPrice=document.createElement("span");
+price.classList.add("price-strike-through");
+price.innerText=`Rs. ${product.oldPrice}`;
+price.appendChild(oldPrice);
+
+
+const discount=document.createElement("span");
+price.classList.add("discount");
+price.innerText=`(${product.discount}% OFF`;
+price.appendChild(discount);
+
+
+// Rating Container
+ const ratings=document.createElement("p");
+ ratings.classList.add("d-flex","align-center")
+
+const rating=document.createElement("span");
+rating.innerText=product.rating;
+ratings.appendChild(rating);
+
+const star=document.createElement("span");
+star.classList.add("material-icons-outlined");
+star.innerText="star";
+ratings.appendChild(star);
+
+cardContainer.appendChild(imageContainer);
+cardContainer.appendChild(cardDetailsContainer);
+    // cardContainer.innerText="Product Card";
+    productContainer.appendChild(cardContainer);
 }
