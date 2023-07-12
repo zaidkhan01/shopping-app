7fc3ed8995cf9d3396f152492d53108cd2bc0123
zaidkhan01 <zaidkhan.a888@gmail.com>
2023-07-12 23:06:28 +0530

    adding product in cart

--- my-ecomm/index.js
+++ my-ecomm/index.js
@@ -1,114 +1,39 @@
 import { products } from "./db/products.js";
-console.log(products)
+import { createProductCard } from "./createProductCard.js";
+import { findProductInCart } from "./utils/findProductInCart.js";
 
-const productContainer=document.getElementById("products");
+const productContainer = document.getElementById("products");
+const filterContainer = document.querySelector(".side-bar");
 
-for(let product of products){
-    const cardContainer=document.createElement("div");
-    cardContainer.classList.add(
-        "card",
-        "card-vertical",
-        "d-flex",
-        "direction-column",
-        "relative",
-        "shadow"
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+productContainer.addEventListener("click", (event) => {
+  const isProductInCart = findProductInCart(cart, event.target.dataset.id);
+  if (!isProductInCart) {
+    const productToAddToCart = products.filter(
+      ({ _id }) => _id === event.target.dataset.id
     );
+    cart = [...cart, ...productToAddToCart];
+    localStorage.setItem("cart", JSON.stringify(cart));
+    const cartButton = event.target;
+    cartButton.innerHTML =
+      "Go To Cart <span class='material-icons-outlined'>shopping_cart</span>";
+  } else {
+    location.href = "cart.html";
+  }
+});
 
+filterContainer.addEventListener("click", (event) => {
+  const updatedProducts = products.filter(
+    ({ rating }) => rating >= Number(event.target.dataset.rating)
+  );
+  productContainer.innerHTML = "";
+  createProductCard(
+    updatedProducts,
+    productContainer,
+    findProductInCart,
+    "products"
+  );
+});
 
-    /*image container */
-    const imageContainer=document.createElement("div");
-    imageContainer.classList.add("card-image-container")
-  
-    const image=document.createElement("img");
-    image.classList.add("card-image");
-    image.setAttribute("src",product.img);
-    image.setAttribute("alt",product.name);
-    imageContainer.appendChild(image);
-
-    /** card details container */
-    const cardDetailsContainer=document.createElement("div");
-    cardDetailsContainer.classList.add("card-details");
-
-    const brandContainer=document.createElement("div");
-    brandContainer.classList.add("card-title");
-    brandContainer.innerText=product.brand;
-    cardDetailsContainer.appendChild(brandContainer)
-
-// card description container
-const descriptionContainer=document.createElement("div");
-descriptionContainer.classList.add("card-description");
-
-const name=document.createElement("p");
-name.classList.add("card-des");
-name.innerText=product.name;
-descriptionContainer.appendChild(name);
-
-// product price
-
-const price=document.createElement("p");
-price.classList.add("card-price","d-flex","align-end","gap-sm");
-price.innerText=`Rs. ${product.newPrice}`;
-
-const oldPrice=document.createElement("span");
-oldPrice.classList.add("price-strike-through");
-oldPrice.innerText=`Rs. ${product.oldPrice}`;
-price.appendChild(oldPrice);
-
-
-const discount=document.createElement("span");
-discount.classList.add("discount");
-discount.innerText=`(${product.discount}% OFF`;
-price.appendChild(discount);
-descriptionContainer.appendChild(price);
-
-// Rating Container
- const ratings=document.createElement("p");
- ratings.classList.add("d-flex","align-center")
-
-const rating=document.createElement("span");
-rating.innerText=product.rating;
-ratings.appendChild(rating);
-
-const star=document.createElement("span");
-star.classList.add("material-icons-outlined","star");
-star.innerText="star";
-ratings.appendChild(star);
-
-descriptionContainer.appendChild(ratings);
-
-cardDetailsContainer.appendChild(descriptionContainer);
-
-//  cta button
-const ctaButton=document.createElement("div");
-ctaButton.classList.add("cta-btn");
-const cartButton=document.createElement("button");
-cartButton.classList.add(
-    "button",
-     "btn-primary",
-     "btn-icon", 
-     "cart-btn" ,
-     "d-flex", 
-     "align-center", 
-     "justify-center", 
-     "gap" ,
-     "cursor", 
-     "btn-margin"
-);
-const cart=document.createElement("span");
-cart.classList.add("material-icons-outlined" ,"star");
-cart.innerText="shopping_cart";
-cartButton.appendChild(cart);
-
-const buttonText=document.createElement("span");
-buttonText.innerText="Add TO Cart";
-cartButton.appendChild(buttonText)
-
-
-ctaButton.appendChild(cartButton);
-cardDetailsContainer.appendChild(ctaButton);
-
-cardContainer.appendChild(imageContainer);
-cardContainer.appendChild(cardDetailsContainer);
-    // cardContainer.innerText="Product Card";
-    productContainer.appendChild(cardContainer);
-}
+createProductCard(products, productContainer, findProductInCart, "products");
